d5a7d01eadabb7bfd244aa942e07e1d355095e00
basillvista <basillvista@gmail.com>
2023-11-28 00:04:37 +0100

    Add visits counter to company

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,22 +2,21 @@
 
 namespace App\Models;
 
+use Awssat\Visits\Visits;
 use Dyrynda\Database\Support\CascadeSoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Service extends Model
+class Company extends Model
 {
     use HasFactory;
     use CascadeSoftDeletes;
     use SoftDeletes;
-
     protected $guarded = ['id'];
 
-    public function serviceAppointments(): HasMany
+    public function vzt(): Visits
     {
-        return $this->hasMany(ServiceAppointment::class);
+        return visits($this);
     }
-
 }
